44d794c2be7f8616f0f88957bb79d7973336411c
Nicolas <nchal@etu.univ-savoie.fr>
2015-02-06 14:09:58 +0100

    initial commit

---
+++ rainbowtable/src/main/java/fr/uds/info006/rainbowtable/App.java
@@ -0,0 +1,13 @@
+package fr.uds.info006.rainbowtable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
